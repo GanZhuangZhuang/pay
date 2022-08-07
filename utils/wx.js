cd2a84ce826c7aac0d55d95412c6b61d52b46dcb
甘壮 <1933024994@qq.com>
2022-08-07 20:10:10 +0800

    轮播图渲染

--- utils/wx.js
+++ utils/wx.js
@@ -1,10 +1,12 @@
-function wxToPromise(method,options){
+function wxToPromise(method = 'request', options = {}){
   return new Promise((resolve,reject)=>{
-    options.success=resolve
-    options.fail=error=>{
+    options.success = resolve
+    options.fail = error =>{
       reject(error)
     }
     wx[method](options)
   })
 }
+
 export default wxToPromise
+
